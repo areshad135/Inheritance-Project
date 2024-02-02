5006246313d9fad24fa4d873f7be9751bb1a35e3
areshad135 <112430364+areshad135@users.noreply.github.com>
2024-02-02 17:03:39 +0000

    draft1

---
+++ polygons.java
@@ -0,0 +1,60 @@
+
+import java.util.*;
+public class polygons {
+
+public static void main(String[] Args) {
+
+
+    Quadrilateral Bob = new Quadrilateral(BOB);
+    Bob.Square();
+
+
+
+
+
+
+
+
+
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+public void polygon{
+
+    String name;
+    int sides;
+
+    define(namea){
+        name = namea
+        sides = sidesa
+    }
+    return name
+    return sides
+
+
+
+}
+
+public void Quadrilaterals extends polygon{
+    super(define);
+    int sidea = 4;
+    Square(namea) {
+        System.out.println(namea + " has " + sidesa + " sides");
+    }
+
+}
